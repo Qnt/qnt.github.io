9414acd24d7c2a8ad854f760ba05c6c5d61c0e56
Ainur <lihodeikvint@gmail.com>
2024-07-08 22:53:07 +0500

    change: yeah, commit today change

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,2 +1,17 @@
 import '../styles/reset.css';
 import '../styles/style.css';
+
+const yankButtons = document.querySelectorAll('.yank-icon');
+
+yankButtons.forEach((yankButton) => {
+  yankButton.addEventListener('click', () => {
+    const anchorEl = yankButton.parentElement.querySelector(
+      '.contact-item__link',
+    );
+    let href = anchorEl.getAttribute('href');
+    if (href.includes('mailto:')) {
+      href = href.replace('mailto:', '');
+    }
+    navigator.clipboard.writeText(href);
+  });
+});
